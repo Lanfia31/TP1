664fbcc08b6ccfe14dd50bd8655a385fd1ad17b0
lanfia <lanfiadiane31@gmail.com>
2024-10-16 15:09:21 +0200

    Ajout de la fonctionnalité de tri des taches

--- task_manager.py
+++ task_manager.py
@@ -12,15 +12,27 @@ def show_tasks():
     for i, task in enumerate(tasks):
       print(f"{i+1}. {task}")
 
-#def mark_task_as_done():
-  # À compléter...
+def mark_task_as_done():
+    print ("Marqué une tâche comme terminée")
+    if not tasks:
+      print("Aucune tâche")
+      return
+def sort_tasks():
+  print("Les tâches sont affichees par odre alphabetique")
+  if not tasks:
+        print("Pas de tâches à afficher")
+  
+  else: 
+    tasks.sort()
+    print("Les tâches sont affichées dans l'ordre alphabetique")
 
 while True:
   print("\nChoisissez une action :")
   print("1. Afficher les tâches")
   print("2. Ajouter une tâche")
   print("3. Marquer une tâche comme terminée")
-  print("4. Quitter")
+  print("4.Trier par ordre alphabétique")
+  print("5. Quitter")
 
   choice = input("> ")
 
@@ -28,9 +40,11 @@ while True:
     show_tasks()
   elif choice == '2':
     add_task()
-  #elif choice == '3':
-    #mark_task_as_done()
+  elif choice == '3':
+    mark_task_as_done()
   elif choice == '4':
+    sort_tasks()
+  elif choice == '5':
     break
   else:
     print("Choix invalide.")
